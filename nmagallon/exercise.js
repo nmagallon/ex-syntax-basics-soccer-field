46058fc05a67635b135bf9b7ad23ae6352522844
Nadia <magallon@mit.edu>
2017-02-06 15:07:00 -0500

    Only passes default ones

---
+++ nmagallon/exercise.js
@@ -0,0 +1,23 @@
+var exercise = {};
+
+exercise.goalCheck = function(x, y) {
+    // log coordinates to the console
+    console.log('x=' + x + ', y=' + y);
+
+    // your task:
+    // complete if statement used to detect
+    // whether center of ball is inside goal area 1
+    if ('check goal 1') {
+        return true;
+    }
+    // your task:
+    // complete if statement used to detect
+    // whether center of ball is inside goal area 2
+    else if ('check goal 2') {
+        return true;
+    }
+
+    return false;
+};
+
+module.exports = exercise;
